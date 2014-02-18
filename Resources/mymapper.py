460042db99830ede63666df4772581859d0d1e7d
Tiago Bortoletto Vaz <tiago@debian.org>
2014-02-18 18:28:43 -0500

    Everything has a begin.

---
+++ Resources/mymapper.py
@@ -0,0 +1,50 @@
+import mapper
+
+class MyMapper:
+    def __init__(self):
+        self.mon = mapper.monitor()
+        self.mon.request_devices()
+        self.mon.request_links_by_src_device_name("/*")
+
+    def poll(self, time):
+        self.mon.poll(time)
+
+    def setLink(self, source, dest, options={}):
+        self.mon.link(source, dest, options)
+
+    def setConnection(self, source, dest, action, options={}):
+        self.mon.poll(50)
+        if action == "connect":
+            self.mon.connect(source, dest, options)
+        elif action == "disconnect":
+            self.mon.disconnect(source, dest)
+
+    def getConnections(self):
+        self.mon.poll(50)
+        self.connections = [ i for i in self.mon.db.all_connections() ]
+        return self.connections
+
+    def getAllDevices(self):
+        self.mon.poll(50)
+        self.devices = [ i["name"] for i in self.mon.db.all_devices() ]
+        return self.devices
+
+    def getInputsFromDevice(self, device_name):
+        self.getAllDevices()
+        inputs_list = [ i for i in self.mon.db.inputs_by_device_name(device_name) ]
+        return inputs_list
+
+    def getOutputsFromDevice(self, device_name):
+        self.getAllDevices()
+        outputs_list = [ i for i in self.mon.db.outputs_by_device_name(device_name) ]
+        return outputs_list
+
+# petit debug
+if __name__ == "__main__":
+    m = MyMapper()
+ #   print m.getAllDevices()
+ #   print m.getInputsFromDevice('/om.1')
+ #   print m.getOutputsFromDevice('/om.1')
+ #   print m.Connect("/om.1", "/om.2")
+    print m.getConnections()
+
---
+++ Resources/panels.py
@@ -0,0 +1,219 @@
+import wx
+import wx.gizmos
+from pyo import *
+import mymapper
+import math
+
+class ConnectionsPanel(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent, style=wx.SUNKEN_BORDER)
+        self.SetBackgroundColour("white")
+        self.Bind(wx.EVT_PAINT, self.OnPaint)
+        self.Bind(wx.EVT_SIZE, self.OnSize)
+        self.my_mapper = mymapper.MyMapper()
+
+    def OnSize(self, evt):
+        wx.CallAfter(self.Refresh)
+
+    def OnPaint(self, evt):
+        self.DrawConnectionsLines()
+
+    def Clear(self):
+        self.dc.Clear()
+
+    def DrawConnectionsLines(self):
+        # loop source and dest trees to get y from each item connected
+        conn_ids = []
+        for c in self.my_mapper.getConnections():
+            src = c["src_name"]
+            dest = c["dest_name"]
+            src_item_id = self.GetParent().GetParent().sources_panel.GetItemByLabel(src, self.GetParent().GetParent().sources_panel.GetRootItem())
+            dest_item_id = self.GetParent().GetParent().destinations_panel.GetItemByLabel(dest, self.GetParent().GetParent().destinations_panel.GetRootItem())
+            conn_ids.append((src_item_id, dest_item_id))
+        w,h = self.GetSize()
+        self.dc = wx.PaintDC(self)
+        gc = wx.GraphicsContext_Create(self.dc)
+        self.dc.Clear()
+        gc.SetPen(wx.Pen("blue", 1))
+        for conn in conn_ids:
+            y1 = self.GetParent().GetParent().sources_panel.getItemPos(conn[0])
+            y2 = self.GetParent().GetParent().destinations_panel.getItemPos(conn[1])
+            # +30 to compensate for the header row
+            l = []
+            y1 = y1[1]+y1[3]/2+26
+            y2 = y2[1]+y2[3]/2+26
+            for i in range(w):
+                mu = float(i)/w
+                mu2 = (1-math.cos(mu*math.pi))/2
+                l.append([i, y1*(1-mu2)+y2*mu2])
+            gc.DrawLines(l)
+        wx.CallAfter(self.Refresh)
+
+class MyTreeList(wx.Panel):
+    def __init__(self, parent, id, which):
+        wx.Panel.__init__(self, parent, -1, style=wx.SUNKEN_BORDER)
+        self.my_mapper = mymapper.MyMapper()
+
+        # sources or destinations tree
+        self.which = which
+
+        # Create the tree
+        self.tree = wx.gizmos.TreeListCtrl(self, style =
+                                           wx.TR_DEFAULT_STYLE
+                                           | wx.TR_FULL_ROW_HIGHLIGHT
+                                           | wx.TR_HAS_BUTTONS + wx.TR_HIDE_ROOT,
+                                           size=(530, 600))
+
+        self.tree.Bind(wx.EVT_TREE_ITEM_ACTIVATED, self.OnActivate, self.tree)
+        self.tree.Bind(wx.EVT_TREE_ITEM_EXPANDED, self.changed, self.tree)
+        self.tree.Bind(wx.EVT_TREE_ITEM_COLLAPSED, self.changed, self.tree)
+        self.Bind(wx.EVT_SIZE, self.OnSize)
+
+        ### expand all
+        expand_icon = wx.ArtProvider.GetBitmap(wx.ART_UNDO, wx.ART_TOOLBAR, (16,16))
+        expand_button = wx.BitmapButton(self, -1, expand_icon, (20, 20), (expand_icon.GetWidth()+10, expand_icon.GetHeight()+10))
+        expand_button.SetToolTipString("This is a bitmap button.")
+        self.Bind(wx.EVT_BUTTON, self.OnExpand, expand_button)
+
+        box = wx.BoxSizer(wx.VERTICAL)
+        box.Add(self.tree, 1)
+
+        # create some columns
+        self.tree.AddColumn("Devices & signals")
+        self.tree.AddColumn("type")
+        self.tree.AddColumn("lengh")
+        self.tree.AddColumn("units")
+        self.tree.AddColumn("minimum")
+        self.tree.AddColumn("maximum")
+        self.tree.SetMainColumn(0) # the one with the tree in it...
+        self.tree.SetColumnWidth(0, 200)
+        self.tree.SetColumnWidth(1, 50)
+        self.tree.SetColumnWidth(2, 50)
+        self.tree.SetColumnWidth(3, 50)
+        self.tree.SetColumnWidth(4, 80)
+
+        # load the tree nodes
+        self.RefreshAll()
+
+        # Expand the first level by default
+        self.tree.ExpandAll(self.root)
+
+    def changed(self, evt):
+        self.GetParent().GetParent().redraw()
+
+    def getItemPos(self, item):
+        while not self.tree.IsVisible(item): # cool!
+            item = self.tree.GetItemParent(item)
+        return self.tree.GetBoundingRect(item)
+
+    def getCurrentItem(self):
+        return self.tree.GetCurrentItem()
+
+    def OnActivate(self, evt):
+        self.NewConnection()
+        self.GetParent().GetParent().connections_panel.DrawConnectionsLines()
+
+    def NewConnection(self):
+        # set link and connection with libmapper
+        self.GetParent().GetParent().my_mapper.setLink("/"+self.GetParent().GetParent().sources_panel.GetSignalAddress().split("/")[1], "/"+self.GetParent().GetParent().destinations_panel.GetSignalAddress().split("/")[1], {})
+        self.GetParent().GetParent().my_mapper.setConnection(self.GetParent().GetParent().sources_panel.GetSignalAddress(), self.GetParent().GetParent().destinations_panel.GetSignalAddress(), action="connect", options={})
+
+    def OnSize(self, evt):
+        self.tree.SetSize(self.GetSize())
+
+
+    #TODO: put some brain here
+    def AddTreeNodes(self, parentItem, items):
+        for item in items:
+            bg_color = 0
+            if self.which == "destinations" and self.my_mapper.getInputsFromDevice(item):
+                newItem = self.tree.AppendItem(parentItem, item)
+                self.tree.SetItemBackgroundColour(newItem, "#D3D3D3")
+                self.tree.SetItemBold(newItem)
+                for i in self.my_mapper.getInputsFromDevice(item):
+                    s_input = self.tree.AppendItem(newItem, i["name"])
+                    if bg_color % 2 == 0:
+                        self.tree.SetItemBackgroundColour(s_input, "#F5F5F5")
+                    bg_color += 1
+                    self.tree.SetItemText(s_input, i["type"], 1)
+                    self.tree.SetItemText(s_input, str(i["length"]), 2)
+                    self.tree.SetItemText(s_input, "", 3)
+                    self.tree.SetItemText(s_input, str(i["min"]), 4)
+                    self.tree.SetItemText(s_input, str(i["max"]), 5)
+
+            elif self.which == "sources" and self.my_mapper.getOutputsFromDevice(item):
+                newItem = self.tree.AppendItem(parentItem, item)
+                self.tree.SetItemBackgroundColour(newItem, "#D3D3D3")
+                self.tree.SetItemBold(newItem)
+                for o in self.my_mapper.getOutputsFromDevice(item):
+                    s_output = self.tree.AppendItem(newItem, o["name"])
+                    if bg_color % 2 == 0:
+                        self.tree.SetItemBackgroundColour(s_output, "#F5F5F5")
+                    bg_color += 1
+                    self.tree.SetItemText(s_output, o["type"], 1)
+                    self.tree.SetItemText(s_output, str(o["length"]), 2)
+                    self.tree.SetItemText(s_output, "", 3)
+                    self.tree.SetItemText(s_output, str(o["min"]), 4)
+                    self.tree.SetItemText(s_output, str(o["max"]), 5)
+
+    def RefreshAll(self):
+        # Add nodes (devices and their signals) to the trees
+        # TODO:
+        # keep last devices list, compare with current and update if changed
+        # same for removing item (DO NOT REBUILD ALL TREE)
+        self.tree.DeleteAllItems()
+        self.root = self.tree.AddRoot("") # hidden root
+        self.devices = self.my_mapper.getAllDevices()
+        self.AddTreeNodes(self.root, self.devices)
+        self.tree.Refresh()
+
+
+    def OnExpand(self, event):
+        self.ExpandAll()
+
+    def ExpandAll(self):
+        self.tree.ExpandAll(self.root)
+
+    def GetRootItem(self):
+        return self.tree.GetRootItem()
+
+    def CollapseAll(self): #TODO: Collapse doesnt work, why? Shouldn't be recursive?
+        self.tree.Collapse(self.tree.GetRootItem())
+
+    def OnCollapse(self, event):
+        self.CollapseAll()
+
+    def GetSelection(self):
+        return self.tree.GetSelection()
+
+    def GetSignalAddress(self): #TODO: make this more readable
+        return str(self.tree.GetItemText(self.tree.GetItemParent(self.tree.GetSelection())) + self.tree.GetItemText(self.tree.GetSelection()))
+
+    def GetItemText(self, item):
+        if item:
+            return self.tree.GetItemText(item)
+        else:
+            return ""
+
+    def OnItemExpanded(self, evt):
+        pass
+
+    def OnItemCollapsed(self, evt):
+        pass
+
+    # HACK: better to store a dict{item_name: item_id} ??
+    def GetItemByLabel(self, search_text, root_item):
+        # receives as search_text a full libmapper address (ex: /device/signal)
+        item, cookie = self.tree.GetFirstChild(root_item)
+        while item.IsOk():
+            text = self.tree.GetItemText(item)
+            if text.lower() == search_text.lower():
+                return item
+            if self.tree.ItemHasChildren(item) and len(search_text.split("/")) > 2:
+                string = "/"+"/".join(search_text.split("/")[2:])
+                match = self.GetItemByLabel(string, item)
+                # if parent is the device, since we can have same signal name for different devices
+                if match.IsOk() and self.tree.GetItemText(self.tree.GetItemParent(match)) == "/"+search_text.split("/")[1]:
+                    return match
+            item, cookie = self.tree.GetNextChild(root_item, cookie)
+        return wx.TreeItemId()
