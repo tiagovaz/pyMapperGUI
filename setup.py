a62e67d11c40a9d659b30945690e54c4db94f0e7
Tiago Bortoletto Vaz <tiago@debian.org>
2014-12-02 13:31:28 -0500

    Change version.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 setup(  name = "pyMapperGUI",
         author = "Tiago Bortoletto Vaz",
         author_email = "tiago@acaia.ca",
-        version = "0.4b",
+        version = "0.1",
         description = "Cross-platform GUI for libmapper",
         url = "https://github.com/tiagovaz/pyMapperGUI",
         license = "GPLv3",
